c6bc7c3d40405e9f61de3841b33cff4a96698d92
mikejeffers <mike.s.jeffers@gmail.com>
2020-08-10 23:01:50 -0400

    actually correct slope calc

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def bound(x, lower=0.0, upper=1.0):
 
 
 def go():
-    arr = np.ndarray(shape=(w, h))
+    arr = np.ndarray(shape=(w, h), dtype=np.int32)
     for x in range(w):
         for y in range(h):
             z_factors = []
@@ -48,17 +48,41 @@ def go():
             z = sum(z_factors) / (sum_weights + gradient_weight)
             z *= math.sin(sq_grad * half_pi)
             z *= pow(sq_grad, 2.0) + 0.5
-            arr[x, y] = int(bound(z) * 64.0)
-
-    save_altitude(arr)
+            arr[x, y] = int(bound(z) * 255.0)
     im = Image.fromarray(arr)
+    save_altitude(arr)
     im.show()
+
+
+def convert_to_altitude(array):
+    for x in range(w):
+        for y in range(h):
+            array[x, y] = int(array[x, y]) << 5
+    return array
+
+
+def compute_slope(x, y, width, height, array):
+    x0 = bound((x + 1), 0, width - 1)
+    y0 = bound((y + 1), 0, height - 1)
+    a, b, c, d = array[x, y], array[x0, y], array[x0, y0], array[x, y0]
+    e = (a + b) >> 1
+    f = (b + c) >> 1
+    g = (c + d) >> 1
+    h = (d + e) >> 1
+    i = (a + b + c + d) >> 2
+    slope_byte = abs(i - a) + abs(i - b) + abs(i - c) + abs(i - d) + abs(i - e) + abs(i - f) + abs(i - g) + abs(i - h)
+    slope_byte *= 2
+    return int(bound(slope_byte, 0, 255))
 
 
 def save_altitude(array):
     with open('altitude.bin', 'wb') as f:
-        for value in np.nditer(array):
-            f.write(struct.pack('>H', int(value)))
+        for x in range(w):
+            for y in range(h):
+                alt = int(array[x, y]) << 5
+                slope = compute_slope(x, y, w, h, array)
+                data = alt | slope
+                f.write(struct.pack('<H', int(data)))
 
 
 if __name__ == '__main__':

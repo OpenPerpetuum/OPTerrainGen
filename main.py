refactors, bug on smooth(), new pipeline and application of gradients

main.py
@@ -5,28 +5,33 @@ from altitude import altitude
 import numpy as np
 import math
 
-SEED = 6
+SEED = 3
+
+ALT_SCALE = 400
 
 half_pi = math.pi / 2.0
 
 w = 512
 h = 512
 
-scales = (200, 125, 75, 45, 35, 25, 15, 7)
-weights = (200, 150, 100, 50, 25, 15, 7, 3)
+# scales = (35, 25, 15, 7, 3, 1)
+# weights = (25, 15, 7, 3, 2, 1)
 
-gradient_weight = 3
-voronoi_weight = 1
-noise_weight = 3
+scales = (200, 125, 75, 45, 35, 25, 15, 7, 3, 1)
+weights = (200, 150, 100, 50, 25, 15, 7, 3, 2, 1)
 
-num_voronoi_pts = utils.bound((w * h) / 10000, 1, 100)
+gradient_weight = 5
+voronoi_weight = 4
+noise_weight = 6
+
+num_voronoi_pts = 100
 
 plex = OpenSimplex(seed=SEED)
 
 
-def go():
+def _old():
     arr = np.ndarray(shape=(w, h), dtype=np.int32)
-    region_z = voronoi_regions(arr)
+    region_z = voronoi_regions(w, h, num_voronoi_pts)
     for x in range(w):
         for y in range(h):
             z_factors = []
@@ -52,21 +57,25 @@ def go():
 
 
 def pipeline():
-    arr = np.ndarray(shape=(w, h), dtype=np.int32)
-    plataeus = voronoi_regions(arr)
-    smoothed_plataeus = smooth(plataeus, 6)
-    noise_terrain = noise()
-    island_terrain = island_gradient()
+    zs = np.ndarray(shape=(w, h), dtype=np.float)
+    grad = smooth(island_gradient(), 5)
+    noi = noise()
+    plataeus = voronoi_regions(w, h, num_voronoi_pts)
+    smoothed_plataeus = smooth(plataeus, 4)
+    for i in range(1):
+        smoothed_plataeus = smooth(smoothed_plataeus, 3)
     for x in range(w):
         for y in range(h):
-            z_factors = []
-            sum_weights = gradient_weight + voronoi_weight + noise_weight
-            z_factors.append(smoothed_plataeus[x, y] * voronoi_weight)
-            z_factors.append(noise_terrain[x, y] * noise_weight)
-            z_factors.append(island_terrain[x, y] * gradient_weight)
-            z = sum(z_factors) / (sum_weights)
-            arr[x, y] = int(utils.bound(z) * 512.0)
+            g = grad[x, y]
+            n = noi[x, y] * noise_weight
+            v = smoothed_plataeus[x, y] * voronoi_weight
+            z = (n + v) / (noise_weight + voronoi_weight)
+            z *= g
+            zs[x, y] = z
+
+    arr = scale_to_bounds(zs, 0.0, 255.0).astype(np.int32)
     im = Image.fromarray(arr)
+    arr = scale_to_bounds(zs, 0.0, ALT_SCALE).astype(np.int32)
     altitude.save_altitude(arr, w, h)
     im.show()
 
@@ -75,31 +84,48 @@ def noise():
     arr = np.ndarray(shape=(w, h), dtype=np.float)
     for x in range(w):
         for y in range(h):
-            z_factors = []
-            sum_weights = sum(weights)
-            for i in range(len(scales)):
-                scale = scales[i]
-                weight = weights[i]
-                noise_val = plex.noise2d(x / scale, y / scale)
-                z_factors.append(noise_val * weight)
-            z = sum(z_factors) / (sum_weights)
-            arr[x, y] = utils.bound(z)
-    return arr
+            arr[x, y] = do_noise_at(x, y, scales, weights)
+    return scale_to_bounds(arr)
 
 
 def island_gradient():
     arr = np.ndarray(shape=(w, h), dtype=np.float)
     for x in range(w):
         for y in range(h):
-            z_factors = []
-            sq_grad = utils.square_gradient(x, y, w, h)
-            sin = math.sin(sq_grad * half_pi)
-            power = pow(sq_grad, 2.0) + 0.5
-            z_factors.append(sin)
-            z_factors.append(power)
-            z = sum(z_factors) / len(z_factors)
-            arr[x, y] = utils.bound(z)
-    return arr
+            arr[x, y] = utils.bound(do_gradient_at(x, y))
+    return scale_to_bounds(arr)
+
+
+def scale_to_bounds(array, lower=0.0, upper=1.0):
+    min_val = array.min()
+    max_val = array.max()
+    starting_domain = max_val - min_val
+    target_domain = upper - lower
+    scale = target_domain / starting_domain
+    array -= (min_val - lower)
+    array *= scale
+    return array
+
+
+def do_gradient_at(x, y):
+    sq_grad = utils.square_gradient(x, y, w, h)
+    cir_grad = utils.circular_gradient(x, y, w, h)
+    grad = sq_grad * cir_grad
+    sin = math.sin(-half_pi + grad * half_pi) / 2.0 + 0.5
+    power = pow(grad, 0.5)
+    return (power + sin) / 2.0
+
+
+def do_noise_at(x, y, scale_list=scales, weight_list=weights):
+    sum_weights = sum(weight_list)
+    z_factors = []
+    for i in range(len(scale_list)):
+        scale = scale_list[i]
+        weight = weight_list[i]
+        noise_val = plex.noise2d(x / scale, y / scale)
+        z_factors.append(noise_val * weight)
+    z = sum(z_factors) / sum_weights
+    return z
 
 
 def smooth(array, radius=2):
@@ -116,11 +142,11 @@ def manhattan_neighborhood(x, y, width, height, radius=1):
     for i in range(-radius, radius + 1):
         x_coord = x + i
         if x_coord < 0 or x_coord >= width:
-            break
+            continue
         for j in range(-radius, radius + 1):
             y_coord = y + j
             if y_coord < 0 or y_coord >= height:
-                break
+                continue
             neighbors.add((x_coord, y_coord))
     return neighbors
 
@@ -145,20 +171,40 @@ def generate_poisson_pts(num_pts=10, margin=(w / 10)):
     return np.stack((x, y), axis=1)
 
 
-def voronoi_regions(array):
-    voronoi_z = np.ndarray(shape=array.shape, dtype=np.float)
-    cell_origins = generate_poisson_pts(num_voronoi_pts)
+def voronoi_regions(width, height, num_regions):
+    voronoi_z = np.ndarray(shape=(width, height), dtype=np.float)
+    cell_origins = generate_poisson_pts(num_regions)
     for x in range(w):
         for y in range(h):
-            min_dist = w * h
-            closest_cell = None
-            for cell_pt in cell_origins:
-                d = utils.dist((x, y), cell_pt)
-                if d <= min_dist:
-                    min_dist = d
-                    closest_cell = cell_pt
-            voronoi_z[x, y] = utils.square_gradient(closest_cell[0], closest_cell[1], w, h)
+            voronoi_z[x, y] = do_voronoi_at(x, y, cell_origins, height_from_average)
     return voronoi_z
+
+
+def height_from_gradient(pt):
+    return do_gradient_at(pt[0], pt[1])
+
+
+def height_from_noise(pt):
+    return do_noise_at(pt[0], pt[1])
+
+
+def height_from_average(pt):
+    g = do_gradient_at(pt[0], pt[1])
+    n = do_noise_at(pt[0], pt[1])
+    return (g + n) / 2.0
+
+
+def do_voronoi_at(x, y, cells, func=height_from_gradient):
+    min_dist = w * h
+    closest_cell = None
+    for cell in cells:
+        d = utils.dist((x, y), cell)
+        if d <= min_dist:
+            min_dist = d
+            closest_cell = cell
+    if closest_cell is None:
+        closest_cell = (x, y)
+    return func(closest_cell)
 
 
 if __name__ == '__main__':
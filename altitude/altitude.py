e0182b57bf79ebc7998fe60c2e55d0656e39f0a9
mikejeffers <mike.s.jeffers@gmail.com>
2020-09-12 15:29:36 -0400

    generate blank layers for non-altitudes

--- altitude/altitude.py
+++ altitude/altitude.py
@@ -1,5 +1,6 @@
 import struct
 from utils import utils
+import os
 
 
 def compute_slope(x, y, width, height, array):
@@ -16,11 +17,33 @@ def compute_slope(x, y, width, height, array):
     return int(utils.bound(slope_byte, 0, 255))
 
 
-def save_altitude(array, width, height):
-    with open('altitude.bin', 'wb') as f:
+def save_all_layers(array, width, height, island_num):
+    island_str = str(island_num).zfill(4)
+    if not os.path.exists(island_str):
+        os.makedirs(island_str)
+    save_empty_layer(width, height, bytes.fromhex('00 00 00 83 00 00 08'), island_str, 'plants', island_str)
+    save_empty_layer(width, height, bytes.fromhex('00 00'), island_str, 'control', island_str)
+    save_empty_layer(width, height, bytes.fromhex('00 00'), island_str, 'blocks', island_str)
+    save_altitude(array, width, height, island_str, island_str)
+
+
+def save_altitude(array, width, height, island_label, folder):
+    file = os.path.join(folder, 'altitude.' + island_label + '.bin')
+    with open(file, 'wb') as f:
         for x in range(width):
             for y in range(height):
                 alt = int(array[x, y]) << 5
                 slope = compute_slope(x, y, width, height, array)
                 data = alt | slope
-                f.write(struct.pack('<H', int(data)))
+                byte_data = data.to_bytes(2, byteorder='little', signed=False)
+                f.write(struct.pack('2s', byte_data))
+
+
+def save_empty_layer(width, height, byte_pattern, island_label, layer_name, folder):
+    file = os.path.join(folder, layer_name + '.' + island_label + '.bin')
+    bytes_len = len(byte_pattern)
+    pack_pattern = str(bytes_len) + 's'
+    with open(file, 'wb') as f:
+        for x in range(width):
+            for y in range(height):
+                f.write(struct.pack(pack_pattern, byte_pattern))
